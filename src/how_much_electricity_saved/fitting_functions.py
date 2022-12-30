786f602f384e231f9a1ff33132b2afbf0cca7e87
Andreas Rydefalk <andreas.rydefalk@iki.fi>
2022-12-30 12:56:47 +0200

    +fitting functions

---
+++ src/how_much_electricity_saved/fitting_functions.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def fitting_func_exp(
+    x: float,
+    a: float,
+    b: float,
+    c: float,
+) -> float:
+    return a * np.exp(b * x) + c
+
+
+def fitting_func_poly3(
+    x: float,
+    a: float,
+    b: float,
+    c: float,
+    d: float,
+) -> float:
+    return a * x**3 + b * x**2 + c * x + d
+
+
+def fitting_func_poly4(
+    x: float,
+    a: float,
+    b: float,
+    c: float,
+    d: float,
+    e: float,
+) -> float:
+    return a * x**4 + b * x**3 + c * x**2 + d * x + e
